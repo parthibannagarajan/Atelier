eded4e3643cdcc4766d8ecc5c0793fdb6d661d11
Parthiban Nagarajan <parthieee1993@gmail.com>
2019-03-15 15:26:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -60,5 +60,9 @@ app.get('*', function(req, res) {
 	res.redirect('/vote');
 });
 
-});
+app.set('port', process.env.PORT || 5000);
 
+// Start node server
+app.listen(app.get('port'), function() {
+	console.log('Node server is running on port ' + app.get('port'));
+});
